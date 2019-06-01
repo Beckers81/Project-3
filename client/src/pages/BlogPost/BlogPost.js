c633a8d2424de2947b775cb7d044090fc642c20d
CERIO18 <ceurioste@derp.org>
2019-06-01 12:46:40 -0600

    update css on blogpost

--- client/src/pages/BlogPost/BlogPost.js
+++ client/src/pages/BlogPost/BlogPost.js
@@ -1,7 +1,12 @@
 import React, { Component } from 'react';
 import "./BlogPost.css";
+import {Form, FormGroup, Input, Label, Col, Container, Row, Jumbotron} from 'reactstrap'
 
 export default class BlogPost extends Component {
+
+
+
+
 
   state = {
     title: '',
@@ -10,19 +15,46 @@ export default class BlogPost extends Component {
 
   render() {
     return (
-      <form>
-        <input
-          type="text"
-          name="title"
-        />
-        <textarea
-          name="body"
-          value={ this.state.title }
-        />
+      
+
+
+      <Container>
+      <Jumbotron>
+      <h1>Please Feel Free to Share Some Positivity!</h1>
+      
+      <Form>
+        {/* <input */}
+          {/* type="text"
+          name="title" */}
+
+          <FormGroup row>
+
+
+          <Label for="exampleText" sm={2}>Tell Us What Brought You Here Today</Label>
+          <Col sm={10}>
+            <Input type="textarea" name="text" id="exampleText" />
+          </Col>
+        </FormGroup>
+        
+        <FormGroup row>
+
+{/* <Jumbotron> */}
+<Label for="exampleText" sm={2}>Please Feel Free to Share Your Positive Thought or Words - Pay Positivity Forward. :) </Label>
+<Col sm={10}>
+  <Input type="textarea" name="text" id="exampleText" className="blogtextarea" bsSize="lg"/>
+</Col>
+</FormGroup>
+
+     
+          {/* name="body" */}
+          {/* value={ this.state.title } */}
+        
 
         <button>Submit</button>
 
-    </form>
+    </Form>
+    </Jumbotron>
+    </Container>
     )
   }
 };
